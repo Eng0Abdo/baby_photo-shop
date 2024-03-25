3f9a6a441846c502891327ecb03b49190591da49
ammarali5 <ammaralighazi5@gmail.com>
2024-03-25 23:11:10 +0200

    filter_3

--- CS112_A3_Part1_S15,16_20230211_20230100_20230246.cpp
+++ CS112_A3_Part1_S15,16_20230211_20230100_20230246.cpp
@@ -94,3 +94,26 @@ void darken_lighten()
     system(photoname.c_str());
 }
 
+
+void invert_image() {
+    string filename;
+    cout << "Pls enter colored image name to turn to gray scale: ";
+    cin >> filename;
+
+    Image image(filename);
+
+    for (int i = 0; i < image.width; ++i) {
+        for (int j = 0; j < image.height; ++j) {
+            for (int k = 0; k < 3; ++k) {
+                image(i, j, k) = 255 - image(i, j, k);
+            }
+        }
+    }
+
+    cout << "Pls enter image name to store new image\n";
+    cout << "and specify extension .jpg, .bmp, .png, .tga: ";
+
+    cin >> filename;
+    image.saveImage(filename);
+
+}
